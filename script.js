61d98cf1b15d23e6a00e3e10f6218e1fb85304e9
oxyzinha <146823768+oxyzinha@users.noreply.github.com>
2024-02-20 14:34:57 +0000

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-// JavaScript function to toggle between languages
-function toggleLanguage() {
-    var portugueseContent = document.getElementById("portuguese-content");
-    var englishContent = document.getElementById("english-content");
-    var button = document.getElementById("language-toggle");
-
-    if (portugueseContent.style.display === "none") {
-        portugueseContent.style.display = "block";
-        englishContent.style.display = "none";
-        button.textContent = "English";
-    } else {
-        portugueseContent.style.display = "none";
-        englishContent.style.display = "block";
-        button.textContent = "Português";
-    }
-}
-
-// Add event listener to the button
-document.getElementById("language-toggle").addEventListener("click", toggleLanguage);
+    <script>
+        function scrollToNextImage(nextImageId) {
+            var nextImage = document.getElementById(nextImageId);
+            nextImage.scrollIntoView({ behavior: 'smooth' });
+        }
+    </script>
